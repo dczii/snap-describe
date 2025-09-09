866ac3c8ae829124fff91ee9297bfba2b7ce907f
Joshclxx <joshuacolobong11@gmail.com>
2025-09-09 23:11:03 +0800

    Added New Components ImageContainer and TabIcon Components

---
+++ frontend/components/cards/ImageContainer.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Image,
+  ImageSourcePropType,
+  ImageStyle,
+  Text,
+  View,
+  ViewStyle,
+  DimensionValue,
+} from "react-native";
+
+interface ImageContainerProps {
+  source: ImageSourcePropType;
+  width?: DimensionValue;
+  height?: DimensionValue;
+  borderRadius?: number;
+  style?: ViewStyle | ViewStyle[];
+  imageStyle?: ImageStyle | ImageStyle[];
+  title?: string;
+  description?: string;
+  price?: number;
+}
+
+const ImageContainer: React.FC<ImageContainerProps> = ({
+  source,
+  width = 100,
+  height = 100,
+  borderRadius = 10,
+  style,
+  imageStyle,
+  title,
+  description,
+  price,
+}) => {
+  return (
+    <View style={[{ width, height, borderRadius, overflow: "hidden" }, style]}>
+      <Image
+        source={source}
+        style={[{ width: "100%", height: "100%", borderRadius }, imageStyle]}
+        resizeMode="cover"
+      />
+    </View>
+  );
+};
+
+export default ImageContainer;
