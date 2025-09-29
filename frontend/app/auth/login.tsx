540cc2a50605552ea2f4097f7e406a739fc7e270
Joshclxx <joshuacolobong11@gmail.com>
2025-09-29 15:52:55 +0800

    Implement API integration for login and registration forms

--- frontend/app/auth/login.tsx
+++ frontend/app/auth/login.tsx
@@ -9,9 +9,58 @@ import {
 import { useRouter } from "expo-router";
 import { Theme, Colors, Gradients } from "@/constants/theme";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+
+type Errors = {
+  email?: string;
+  password?: string;
+};
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
+
+  const [errors, setErrors] = useState<Errors>({});
+
+  const validateForm = () => {
+    const newErrors: Errors = {};
+
+    if (!email.trim()) newErrors.email = "Required to fill.";
+    else if (!/\S+@\S+\.\S+/.test(email))
+      newErrors.email = "Invalid email format.";
+    if (!password.trim()) newErrors.password = "Required to fill.";
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(
+        "https://snap-describe-production.up.railway.app/v1/auth/mobile/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Login Successfully");
+        router.replace("/(tabs)");
+      } else {
+        const message =
+          data.message !== "Incorrect email or password."
+            ? "Login failed"
+            : data.message;
+        alert(message);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <View style={styles.wrapper}>
@@ -22,17 +71,25 @@ export default function Login() {
           style={styles.input}
           placeholder="Email or Phone"
           placeholderTextColor={Theme.text}
+          value={email}
+          onChangeText={setEmail}
         />
+        {errors.email && <Text style={styles.errorFull}>{errors.email}</Text>}
         <TextInput
           style={styles.input}
           placeholder="Password"
           placeholderTextColor={Theme.text}
           secureTextEntry
+          value={password}
+          onChangeText={setPassword}
         />
+        {errors.email && (
+          <Text style={styles.errorFull}>{errors.password}</Text>
+        )}
 
         <TouchableOpacity
           activeOpacity={0.8}
-          onPress={() => router.replace("/(tabs)")}
+          onPress={handleLogin}
           style={{ width: "100%", marginTop: 16 }}
         >
           <LinearGradient
@@ -72,6 +129,7 @@ export default function Login() {
 }
 
 const styles = StyleSheet.create({
+  // CONTAINERS
   wrapper: {
     flex: 1,
     justifyContent: "center",
@@ -84,6 +142,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 12,
     alignItems: "center",
+
+    // SHADOW STYLING
     shadowColor: Theme.primary,
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
@@ -91,7 +151,6 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   gmailContainer: {
-    // padding: 6,
     paddingVertical: 6,
     marginTop: 16,
     width: "80%",
@@ -100,19 +159,36 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     flexDirection: "row",
     alignItems: "center",
+    gap: 6,
+
     shadowColor: Theme.primary,
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     elevation: 5,
-    gap: 6,
   },
+
+  // TYPOGRAPHY
   title: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 24,
     color: Colors.text,
   },
+  errorFull: {
+    color: Colors.error,
+    fontSize: 14,
+    marginTop: 4,
+    flexWrap: "wrap",
+    width: "100%",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  // INPUTS
   input: {
     borderWidth: 1,
     borderColor: Theme.primary,
@@ -123,6 +199,8 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 16,
   },
+
+  // BUTTONS
   button: {
     backgroundColor: Theme.primary,
     paddingVertical: 12,
@@ -131,10 +209,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     marginTop: 16,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
   },
 });
--- frontend/app/auth/register.tsx
+++ frontend/app/auth/register.tsx
@@ -9,8 +9,92 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
+import { useState } from "react";
+
+type Errors = {
+  fullname?: string;
+  email?: string;
+  phoneNumber?: string;
+  password?: string;
+  confirmPassword?: string;
+};
 
 export default function Register() {
+  const [fullname, setFullname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [errors, setErrors] = useState<Errors>({});
+
+  const validateForm = (): boolean => {
+    const newErrors: Errors = {};
+
+    if (!fullname.trim()) newErrors.fullname = "Required to fill.";
+
+    if (!email.trim()) newErrors.email = "Required to fill.";
+    else if (!/\S+@\S+\.\S+/.test(email))
+      newErrors.email = "Invalid email format.";
+
+    const phoneRegex = /^(\+63|0)\d{10}$/;
+    if (!phoneNumber.trim()) {
+      newErrors.phoneNumber = "Required to fill.";
+    } else if (!phoneRegex.test(phoneNumber)) {
+      newErrors.phoneNumber = "Invalid phone number format.";
+    }
+
+    if (!password) {
+      newErrors.password = "Required to fill.";
+    } else if (
+      !/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])(?=.{8,}).*$/.test(password)
+    ) {
+      newErrors.password =
+        "Password must be 8+ characters, include 1 uppercase, 1 number, and 1 special character";
+    }
+
+    if (!confirmPassword) {
+      newErrors.confirmPassword = "Required to fill.";
+    } else if (password !== confirmPassword) {
+      newErrors.confirmPassword = "Password not match.";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleRegister = async () => {
+    if (!validateForm()) return;
+
+    try {
+      const response = await fetch(
+        "https://snap-describe-production.up.railway.app/v1/auth/mobile/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ fullname, phoneNumber, email, password }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Registered successfully");
+        router.replace("/auth/login");
+      } else {
+        const message =
+          data.message !== "Incorrect email or password."
+            ? "Register failed"
+            : data.message;
+        alert(message);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <View style={styles.containerTop}>
       <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
@@ -29,7 +113,12 @@ export default function Register() {
             style={styles.input}
             placeholder="Juan Dela Cruz"
             placeholderTextColor={Colors.placeholder}
+            value={fullname}
+            onChangeText={setFullname}
           />
+          {errors.fullname && (
+            <Text style={styles.errorFull}>{errors.fullname}</Text>
+          )}
         </View>
 
         <View>
@@ -38,44 +127,57 @@ export default function Register() {
             style={styles.input}
             placeholder="juandelacruz@gmail.com"
             placeholderTextColor={Colors.placeholder}
+            value={email}
+            onChangeText={setEmail}
           />
+          {errors.email && <Text style={styles.errorFull}>{errors.email}</Text>}
         </View>
 
         <View>
-          <Text style={styles.label}>Phone +</Text>
+          <Text style={styles.label}>Phone</Text>
           <TextInput
             style={styles.input}
             placeholder="0917-218-6677"
             placeholderTextColor={Colors.placeholder}
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
           />
+          {errors.phoneNumber && (
+            <Text style={styles.errorFull}>{errors.phoneNumber}</Text>
+          )}
         </View>
 
-        <View style={styles.inputBetween}>
-          <View style={styles.field}>
-            <Text style={styles.label}>Password</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter Password"
-              placeholderTextColor={Colors.placeholder}
-              secureTextEntry
-            />
-          </View>
-
-          <View style={styles.field}>
-            <Text style={styles.label}>Confirm Password</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Re-enter Password"
-              placeholderTextColor={Colors.placeholder}
-              secureTextEntry
-            />
-          </View>
+        <View>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Password"
+            placeholderTextColor={Colors.placeholder}
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+          {errors.password && (
+            <Text style={styles.errorFull}>{errors.password}</Text>
+          )}
         </View>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.replace("/(tabs)")}
-        >
+        <View>
+          <Text style={styles.label}>Confirm Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Re-enter Password"
+            placeholderTextColor={Colors.placeholder}
+            secureTextEntry
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+          {errors.confirmPassword && (
+            <Text style={styles.errorFull}>{errors.confirmPassword}</Text>
+          )}
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={handleRegister}>
           <Text style={styles.buttonText}>Register</Text>
         </TouchableOpacity>
       </LinearGradient>
@@ -84,6 +186,7 @@ export default function Register() {
 }
 
 const styles = StyleSheet.create({
+  //  CONTAINERS
   containerTop: {
     flex: 1,
     justifyContent: "center",
@@ -93,17 +196,19 @@ const styles = StyleSheet.create({
   container: {
     margin: 16,
     paddingHorizontal: 24,
-    paddingTop: 64,
-    paddingBottom: 64,
+    paddingTop: 32,
+    paddingBottom: 32,
     width: "100%",
-    height: "auto",
     backgroundColor: Theme.primary,
     borderRadius: 12,
     gap: 20,
 
+    // SHADOW STYLING
     elevation: 10,
     shadowColor: "white",
   },
+
+  //  NAVIGATION / ICONS
   backButton: {
     position: "absolute",
     top: 40,
@@ -114,11 +219,27 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
   },
+
+  //  TYPOGRAPHY
   label: {
     fontSize: 16,
     marginBottom: 8,
     color: "white",
   },
+  errorFull: {
+    color: Colors.error,
+    fontSize: 14,
+    marginTop: 4,
+    flexWrap: "wrap",
+    width: "100%",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+
+  //  INPUT FIELDS
   input: {
     borderWidth: 1,
     borderColor: "#ccc",
@@ -137,17 +258,12 @@ const styles = StyleSheet.create({
   field: {
     flex: 1,
   },
+
+  //  BUTTONS
   button: {
     backgroundColor: Theme.primary,
     borderRadius: 50,
-    color: "white",
     padding: 12,
-
     marginTop: 32,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    textAlign: "center",
   },
 });
